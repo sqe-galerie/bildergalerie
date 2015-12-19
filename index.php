03840954a495f583264cc90bbf59c12524c4c541
Felix Blechschmitt <8507319+felixble@users.noreply.github.com>
2015-12-19 14:05:38 +0000

    index.php cleaned

--- index.php
+++ index.php
@@ -1,24 +1,6 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-//=== Check cookies enabled ===//
-/*session_start();
-$a = session_id();
-session_destroy();
-
-session_start();
-$b = session_id();
-session_destroy();
-$cookiesEnabled = $a==$b;
-//=== Check cookies enabled ===//
-*/
-
-
-/*$dispatcher = new Dispatcher($_GET, $_POST);
-$ctrl = $dispatcher->handle();
-echo $ctrl->draw();
-*/
-
 $request = new Request($_SERVER["REQUEST_URI"], $_GET, $_POST, $_SERVER["REQUEST_METHOD"]);
 
 $router = new Router($request);
