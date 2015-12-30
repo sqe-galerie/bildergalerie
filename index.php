56295f5e5f84b12c0282b6381b84150f8eccc57f
Felix Blechschmitt <8507319+felixble@users.noreply.github.com>
2015-12-30 18:00:31 +0000

    more comments, more win

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
 <?php
+// include the autoloader-script which is responsible to include all necessary class files
 require __DIR__ . '/vendor/autoload.php';
 
+/**
+ * Function to convert all php errors
+ * (except fatal errors) into an exception
+ * so they will be displayed in our exception view.
+ *
+ * @param $errno
+ * @param $errstr
+ * @param $errfile
+ * @param $errline
+ * @throws ErrorHandler
+ */
 function exception_error_handler($errno, $errstr, $errfile, $errline ) {
     throw new ErrorHandler($errstr, 0, $errno, $errfile, $errline);
 }
+
+// set our error to exception converter as global exception handler
 set_error_handler("exception_error_handler", E_ALL);
 
-
+// build the request object containing the request uri, all get and post parameters together
+// with the cookies array
 $request = new Request($_SERVER["REQUEST_URI"], $_GET, $_POST, $_SERVER["REQUEST_METHOD"], $_COOKIE);
+
+// create a new Router-Object which is responsible to determine the correct controller and action-method
+// from the given request.
 $router = new Router($request);
 
+// print the return value from the executed action, which is the desired view.
 echo $router->run();
