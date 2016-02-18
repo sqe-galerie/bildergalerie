905aaaba43f7c52afe4f8184e4ad86e055dd3e4e
Felix Blechschmitt <8507319+felixble@users.noreply.github.com>
2016-02-18 18:51:23 +0000

    picture form design

--- templates/picture_form.php
+++ templates/picture_form.php
@@ -1,26 +1,31 @@
-<form class="form-horizontal" role="form">
-    <div class="form-group">
-        <label class="control-label col-sm-2" for="email">Email:</label>
-        <div class="col-sm-10">
-            <input type="email" class="form-control" id="email" placeholder="Enter email">
+<form role="form">
+
+    <div class="row">
+
+        <div class="col-lg-4">
+            <h1>Bild hochladen</h1>
+
         </div>
-    </div>
-    <div class="form-group">
-        <label class="control-label col-sm-2" for="pwd">Password:</label>
-        <div class="col-sm-10">
-            <input type="password" class="form-control" id="pwd" placeholder="Enter password">
-        </div>
-    </div>
-    <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-            <div class="checkbox">
-                <label><input type="checkbox"> Remember me</label>
+
+
+        <div class="col-lg-8">
+
+            <h1>Informationen hinzufügen</h1>
+
+            <div class="form-group">
+                <label for="email">Titel</label>
+                <input type="email" class="form-control" id="email">
             </div>
+            <div class="form-group">
+                <label for="description">Beschreibung:</label>
+                <textarea class="form-control" rows="5" id="description"></textarea>
+            </div>
+            <button type="submit" class="btn btn-success">Submit</button>
+
+
         </div>
+
     </div>
-    <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn btn-default">Submit</button>
-        </div>
-    </div>
+
+
 </form>
