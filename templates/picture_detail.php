48b080aa2cfdc80a44c9e90e10f133c251baea8a
Felix Blechschmitt <8507319+felixble@users.noreply.github.com>
2016-02-23 22:38:22 +0000

    Pic details show title

--- templates/picture_detail.php
+++ templates/picture_detail.php
@@ -1,4 +1,5 @@
 <?php /** @var Picture_detailView $this */ ?>
-<div class="center-block">
+<div class="text-center">
 <img src="<?php echo $this->getPicture()->getPath()->getPath(); ?>" class="img-rounded img-responsive center-block" alt="Cinque Terre" width="800" />
+    <h1><?php echo $this->getPicture()->getTitle(); ?></h1>
 </div>
