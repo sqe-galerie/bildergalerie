d49dc2cef6f6edb192b06c4969f9767687a582ac
Felix Blechschmitt <8507319+felixble@users.noreply.github.com>
2016-02-24 23:13:40 +0000

    picture detail

--- templates/picture_detail.php
+++ templates/picture_detail.php
@@ -1,7 +1,16 @@
 <?php /** @var Picture_detailView $this */ ?>
-<div class="text-center">
-<img src="<?php echo $this->getPicture()->getPath()->getPath(); ?>"
-     class="img-rounded img-responsive center-block"
+<div class="row">
+    <div class="col-lg-9">
+        <img src="<?php echo $this->getPicture()->getPath()->getPath(); ?>"
+             class="img-rounded img-responsive center-block
      alt="<?php echo $this->getPicture()->getTitle(); ?>" width="800" />
-    <h1><?php echo $this->getPicture()->getTitle(); ?></h1>
+
+    </div>
+    <div class="col-lg-3">
+        <h3><?php echo $this->getPicture()->getTitle(); ?></h3>
+        <?php echo $this->getPicture()->getCategory()->getCategoryName(); ?><br>
+        <?php echo $this->getPicture()->getMaterial(); ?><br>
+        <p>
+        <?php echo $this->getPicture()->getDescription(); ?></p>
+    </div>
 </div>
