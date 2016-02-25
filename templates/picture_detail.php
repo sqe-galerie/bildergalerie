67d590871b805b69c4bc6b7bfa83b7928a72660b
Felix Blechschmitt <8507319+felixble@users.noreply.github.com>
2016-02-25 08:39:23 +0000

    picture detail more infos

--- templates/picture_detail.php
+++ templates/picture_detail.php
@@ -1,16 +1,19 @@
 <?php /** @var Picture_detailView $this */ ?>
 <div class="row">
-    <div class="col-lg-9">
+    <div class="col-lg-8">
+        <a href="#">Zurück zur Übersicht</a>
         <img src="<?php echo $this->getPicture()->getPath()->getPath(); ?>"
              class="img-rounded img-responsive center-block
      alt="<?php echo $this->getPicture()->getTitle(); ?>" width="800" />
 
     </div>
-    <div class="col-lg-3">
+    <div class="col-lg-4">
         <h3><?php echo $this->getPicture()->getTitle(); ?></h3>
-        <?php echo $this->getPicture()->getCategory()->getCategoryName(); ?><br>
-        <?php echo $this->getPicture()->getMaterial(); ?><br>
-        <p>
-        <?php echo $this->getPicture()->getDescription(); ?></p>
+        <h4>Material/Technik</h4>
+        <?php echo $this->getPicture()->getMaterial(); ?>
+        <h4>Format</h4>
+        <?php echo $this->getPicture()->getFormat(); ?>
+        <h4>Beschreibung</h4>
+        <p><?php echo $this->getPicture()->getDescription(); ?></p>
     </div>
 </div>
