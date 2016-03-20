c34ff4e34e695840a69e1873996c30bdb8689b6c
marc <otting.marc@gmail.com>
2016-03-20 11:19:18 +0000

    erweiterung der newsview mit panel zur darstellung der kommentare

--- templates/post_news.php
+++ templates/post_news.php
@@ -21,3 +21,27 @@
     </div>
     <div class="col-md-2"></div>
 </div>
+
+<div class="row">
+
+    <div class="col-md-2"></div>
+
+    <div class="col-md-8">
+        <div class="panel panel-default">
+            <div class="panel-heading">Panel heading without title</div>
+            <div class="panel-body">
+                Panel content
+            </div>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Panel title</h3>
+            </div>
+            <div class="panel-body">
+                Panel content
+            </div>
+        </div>
+    </div>
+    <div class="col-md-2"></div>
+</div>
