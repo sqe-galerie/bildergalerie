9f84b4f1d0cd86068cdf6f2a1086739738b28736
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2017-12-30 18:03:57 +0100

    Vagrant: run `composer install` after launching the vm

--- vagrant/after-up.sh
+++ vagrant/after-up.sh
@@ -2,6 +2,11 @@
 
 MYSQL_USER='homestead'
 MYSQL_PW='secret'
+VM_SYNC_FOLDER='/vagrant_data'
 
 sudo echo "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));"  | \
     mysql --user=${MYSQL_USER} --password=${MYSQL_PW}
+
+# run composer and install dependencies
+cd ${VM_SYNC_FOLDER}
+composer install
