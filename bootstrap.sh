c566d30a2b1f71953e7124dc03eb20ff1ec23775
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2017-12-30 17:00:05 +0100

    first working custom vagrant file
    details see: http://adamjstevenson.com/tutorials/2015/12/27/a-simple-lamp-dev-environment-with-vagrant.html
    
    Issues:
    how to access the mysql database from the host?

---
+++ bootstrap.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Use single quotes instead of double quotes to make it work with special-character passwords
+PASSWORD='secret'
+
+# update / upgrade
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# install apache 2.5 and php 5.5
+sudo apt-get install -y apache2
+sudo apt-get install -y php5
+
+# install mysql and give password to installer
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+sudo apt-get -y install mysql-server
+sudo apt-get install php5-mysql
+
+# setup hosts file
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+    DocumentRoot "/var/www/html/"
+    <Directory "/var/www/html/">
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+
+# enable mod_rewrite
+sudo a2enmod rewrite
+
+# restart apache
+service apache2 restart
+
+# install git
+sudo apt-get -y install git
+
+# install curl
+sudo apt-get -y install curl
+
+# install nano
+sudo apt-get -y install nano
+
+# install Composer
+curl -s https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
