4118357cd3cc162ab7d8a956d1c3a91c5fe6b693
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2018-01-18 16:47:22 +0100

    Travis: Try to give travis user privileges for mysql db
    Try to fix the following error on travis:
    ```
    $ ./travis-setup-db.sh
    Warning: Using a password on the command line interface can be insecure.
    ERROR 1045 (28000): Access denied for user 'travis'@'localhost' (using password: YES)
    Warning: Using a password on the command line interface can be insecure.
    ERROR 1045 (28000): Access denied for user 'travis'@'localhost' (using password: YES)
    Warning: Using a password on the command line interface can be insecure.
    ERROR 1045 (28000): Access denied for user 'travis'@'localhost' (using password: YES)
    Warning: Using a password on the command line interface can be insecure.
    ERROR 1045 (28000): Access denied for user 'travis'@'localhost' (using password: YES)
    ```

--- travis-setup-db.sh
+++ travis-setup-db.sh
@@ -5,6 +5,9 @@ MYSQL_PW='root'
 MYSQL_DB='sqe_bildergalerie'
 
 # Setup database
+mysql -u root -e "CREATE DATABASE sqe_bildergalerie;"
+mysql -u root -e "GRANT ALL PRIVILEGES ON sqe_bildergalerie.* TO 'travis'@'%';";
+
 sudo echo "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));"  | \
     mysql --user=${MYSQL_USER} --password=${MYSQL_PW}
 sudo echo "DROP DATABASE IF EXISTS sqe_bildergalerie" | \
