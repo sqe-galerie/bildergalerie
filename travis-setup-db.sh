3c4f19c4907bf297a1c142e514ae6641830ee701
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2018-01-19 09:33:49 +0100

    Travis-CI: Set correct mysql password
    Should reed the docs more carefully:
    > You can connect using the username travis or root and a blank password.
    https://docs.travis-ci.com/user/database-setup/#MySQL

--- travis-setup-db.sh
+++ travis-setup-db.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
 MYSQL_USER='travis'
-MYSQL_PW='root'
+MYSQL_PW=''
 MYSQL_DB='sqe_bildergalerie'
 
 # Setup database
-mysql -u root -e "CREATE DATABASE sqe_bildergalerie;"
-mysql -u root -e "GRANT ALL PRIVILEGES ON sqe_bildergalerie.* TO 'travis'@'%';";
-
 sudo echo "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));"  | \
     mysql --user=${MYSQL_USER} --password=${MYSQL_PW}
 sudo echo "DROP DATABASE IF EXISTS sqe_bildergalerie" | \
