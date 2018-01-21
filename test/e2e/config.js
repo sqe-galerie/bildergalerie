c576ebe6858844e4cd2bd564c3efad84158765da
Dominik Becker <dobecker93@gmail.com>
2018-01-21 23:17:02 +0100

    npm script for running e2e tests

--- test/e2e/config.js
+++ test/e2e/config.js
@@ -1,1 +1,3 @@
-export const SERVER_URL = "http://localhost:8888";
+//export const SERVER_URL = "http://localhost:8888";
+export const SERVER_URL = process.env.SERVER_URL || "http://localhost:8888";
+
--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -9,7 +9,7 @@ php -S 0.0.0.0:8888 -t ../../  >& /dev/null &
 SERVER_PID=$!
 
 # run the e2e tests
-testcafe all test/**/*.test.js
-
+#$(npm bin)/testcafe all test/**/*.test.js
+SERVER_URL=http://5.5.5.5 $(npm bin)/testcafe chrome test/**/*.test.js
 # shutdown the server
 kill $SERVER_PID
