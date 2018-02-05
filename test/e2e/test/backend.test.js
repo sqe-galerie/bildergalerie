24945c9e5da6b05ce1ddd92641c072be5626978a
burpmansys <33862965+burpmansys@users.noreply.github.com>
2018-02-05 19:08:26 +0100

    e2e test to update exhibition
    Added e2e test to update exhibition data (title, description)

--- test/e2e/test/backend.test.js
+++ test/e2e/test/backend.test.js
@@ -23,3 +23,25 @@ test('erstelle eine neue Ausstellung', async t => {
     // step 3 - validate results
     await tabAustellungen.checkAusstellungWirdAngezeigt(uniqueName);
 });
+
+test('update von Ausstellungs Daten', async t => {
+    // step 1 - generate values
+	const uniqueName = "Ausstellung_" + (new Date().getTime());
+
+    // step 2 - perform steps
+    await t.useRole(DEMO_USER);
+    let page = await PageBackend(t).navigateToViaNavbar();
+    let tabAustellungen = await page.tabs.chooseAusstellungen();
+    let dialog = await tabAustellungen.clickNeueAustellung();
+    await dialog.setTitel(uniqueName);
+    await dialog.setBeschreibung("Beschreibung vor Aenderung");
+    await dialog.clickAnlegen();
+	let changeDialog = await tabAustellungen.clickEditAusstellung(uniqueName);
+	await changeDialog.setTitel(uniqueName + '_changed');
+	await dialog.setBeschreibung("Beschreibung nach Aenderung");
+	await dialog.clickAnlegen();
+	
+    
+    // step 3 - validate results
+    await tabAustellungen.checkAusstellungWirdAngezeigt(uniqueName + '_changed');
+});
