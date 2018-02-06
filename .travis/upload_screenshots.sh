499650702a7aded2034d43b8c06eba4e598e3b2f
Jan <jasteuer@htwsaar.de>
2018-02-06 02:09:59 +0100

    better upload script

--- .travis/upload_screenshots.sh
+++ .travis/upload_screenshots.sh
@@ -1,12 +1,17 @@
-#!/bin/bash
+#!/bin/bash 
+cd $( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 TARGET_FOLDER="jasteuer@stl-s-stud.htwsaar.de:/export/home_16/jasteuer/public_html/sqe"
 FINAL_BASE_LINK="http://www1.htwsaar.de/~jasteuer/sqe"
 FINAL_TUNNEL_LINK="localhost:9000/~jasteuer/sqe"
-
-for FILE in ../_screenshots/*; do 
-    NAME=$(basename $FILE);
-    echo "uploading screenshot to: $FINAL_BASE_LINK/$NAME"; 
-    echo "                         $FINAL_TUNNEL_LINK/$NAME"; 
-    scp -i id_rsa $FILE "$TARGET_FOLDER/$NAME" 
-done
+ 
+if [ -d "../screenshots" ]; then
+    for FILE in ../screenshots/*; do 
+        if [ -d "$FILE" ]; then
+            NAME=$(basename $FILE);
+            echo "uploading screenshot to: $FINAL_BASE_LINK/$NAME"; 
+            echo "                         $FINAL_TUNNEL_LINK/$NAME"; 
+            scp -i id_rsa $FILE "$TARGET_FOLDER/$NAME" 
+        fi
+    done
+fi
