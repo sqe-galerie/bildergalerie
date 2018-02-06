0c251b45e1c8d677d3243682d86ea09324f3b1c9
jsteuer <jsteuer@htwsaar.de>
2018-02-06 12:20:43 +0100

    not screenshots/screenshots...

--- .travis/upload_screenshots.sh
+++ .travis/upload_screenshots.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 cd $( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-TARGET_FOLDER="jasteuer@stl-s-stud.htwsaar.de:/export/home_16/jasteuer/public_html/sqe/screenshots"
+TARGET_FOLDER="jasteuer@stl-s-stud.htwsaar.de:/export/home_16/jasteuer/public_html/sqe"
 FINAL_BASE_LINK="http://www1.htwsaar.de/~jasteuer/sqe/screenshots"
 FINAL_TUNNEL_LINK="localhost:9000/~jasteuer/sqe/screenshots"
  
