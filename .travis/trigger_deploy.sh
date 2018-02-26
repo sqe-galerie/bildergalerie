863c8531dc994cbb4552d6a61416319e57770753
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2018-02-26 23:41:28 +0100

    CD: echo some information when deploying (or not)

--- .travis/trigger_deploy.sh
+++ .travis/trigger_deploy.sh
@@ -2,7 +2,9 @@
 
 
 if [ ${TRAVIS_BRANCH} != "develop" ]; then
+    echo "Skip deployment. Not on develop branch."
     exit
 fi
 
+echo "Trigger deployment..."
 curl "http://deploy.hildes-bildergalerie.de/?token=${deploy_token}"
