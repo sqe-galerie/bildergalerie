f27a36462f43797097e2ee726b744e381fcc67f9
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2018-03-02 11:43:22 +0100

    Fix: UploadPicture Unit test -> use PictureUploader stub

--- test/unit/classes/app/picture/upload/UploadPictureTest.php
+++ test/unit/classes/app/picture/upload/UploadPictureTest.php
@@ -3,8 +3,7 @@
 namespace App\Picture\Upload;
 
 use App\Picture\PictureRepository;
-use crodas\FileUtil\File;
-use PHPUnit\Exception;
+use App\Picture\PictureUploader;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
@@ -27,12 +26,18 @@ class UploadPictureTest extends TestCase
     private $userStub;
 
     /**
+     * @var PictureUploader | MockObject
+     */
+    private $picturerUploaderStub;
+
+    /**
      * @before
      */
     public function setupTestobject()
     {
         $this->repositoryStub = $this->createMock(PictureRepository::class);
-        $this->uploadInteractor = new UploadPicture($this->repositoryStub);
+        $this->picturerUploaderStub = $this->createMock(PictureUploader::class);
+        $this->uploadInteractor = new UploadPicture($this->repositoryStub, $this->picturerUploaderStub);
         $this->mandantStub = $this->createMock(\Mandant::class);
         $this->userStub = $this->createMock(\User::class);
     }
@@ -44,6 +49,9 @@ class UploadPictureTest extends TestCase
             "name" => "twitter"
         );
         $dir = "upload";
+        $this->picturerUploaderStub
+            ->method('uploadFile')
+            ->willReturn(true);
         $this->repositoryStub
             ->expects($this->once())
             ->method('uploadPicture');
